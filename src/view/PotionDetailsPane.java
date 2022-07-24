ee1de01cfea08b448cf15415ea51f0b791b2339d
mattsmartey13 <matthew.victor.smart96@gmail.com>
2022-07-24 17:25:30 +0100

    SAR-31 now generating potions, ingredient combinations are operational and being formed correctly

---
+++ src/view/PotionDetailsPane.java
@@ -0,0 +1,11 @@
+package view;
+
+import javax.swing.*;
+
+public class PotionDetailsPane extends Popup {
+    private String potionName, potionValue, potionEffects;
+
+    public PotionDetailsPane() {
+
+    }
+}
